49625969841182cbb42308969373a0d6012a9d0d
Valentina Vasco <valentina.vasco@iit.it>
2020-10-21 12:31:08 +0200

    [smoke-test] updated pre and post-test

--- smoke-test/post-test.sh
+++ smoke-test/post-test.sh
@@ -5,12 +5,3 @@
 # CopyPolicy: Released under the terms of the GNU GPL v3.0.
 
 # Put here those instructions we need to execute after having run the test
-
-if [ $stop_yarpserver == "yes" ]; then 
-    echo "stop_yarpserver == yes killing it "
-    killall -9 yarpserver 
-fi
-
- 
-
-unset stop_yarpserver
--- smoke-test/pre-test.sh
+++ smoke-test/pre-test.sh
@@ -5,14 +5,3 @@
 # CopyPolicy: Released under the terms of the GNU GPL v3.0.
 
 # Put here those instructions we need to execute before running the test
-
-yarp where
-if [ $? -eq 0 ]; then
-   export stop_yarpserver="no"
-   echo "stop_yarpserver == no"
-else
-   export stop_yarpserver="yes"
-   echo "stop_yarpserver == yes"
-   yarpserver --write &
-   sleep 1
-fi
